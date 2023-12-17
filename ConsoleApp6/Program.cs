18ef63770c7a5332a193ca0ae3fc5b7f268d8835
Your Name <you@example.com>
2023-12-17 13:34:46 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var tuple = Tuple.Create("admin7", "World135");
+
+string entusername;
+string entpassword;
+do
+{
+    Console.WriteLine("Enter your username");
+    entusername = Console.ReadLine();
+    Console.WriteLine("Enter your password");
+    entpassword = Console.ReadLine();
+
+    if (tuple.Item1 != entusername || tuple.Item2 != entpassword)
+    {
+        Console.WriteLine("Your entered username or password is not correct.Try again!");
+
+    }
+   else
+    {
+        Console.WriteLine("You signed in successfully");
+        break;
+    }
+}
+while (tuple.Item1 != entusername || tuple.Item2 != entpassword);
+
+
+
